10ce2edbf3e8be9c0f7b397c9f4f035a69f90f4a
troingle <bugsinsidemyhead@gmail.com>
2023-08-09 18:00:11 +0300

    Added variable jumping and made dashing more snappy

--- Player.gd
+++ Player.gd
@@ -1,12 +1,16 @@
-
 extends KinematicBody2D
 
-const MOVE_SPEED = 420
-const DASH_SPEED = 8
-const JUMP_FORCE = 850
+const MOVE_SPEED = 500
+const DASH_SPEED = 11
+const JUMP_FORCE = 600  # Adjusted jump force for snappy jumps
 const GRAVITY = 50
 const MAX_FALL_SPEED = 1000
 const COYOTE_TIME = 0.2
+
+const MIN_JUMP_TIME = 0.001
+const MAX_JUMP_TIME = 0.2
+var jump_timer = 0
+var is_jumping = false
 
 onready var sprite = $Sprite
 
@@ -28,28 +32,41 @@ func _physics_process(delta):
 			move_dir += DASH_SPEED
 		else:
 			move_dir -= DASH_SPEED
+		jump_timer = 0
+		y_velo = 0
 		can_dash = false
 	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
 	
 	if grounded:
 		coyote_timer = COYOTE_TIME
 		can_dash = true
+		is_jumping = false
+		jump_timer = 0
 	else:
 		coyote_timer -= delta
-		
+	
 	if (facing_right and move_dir < 0) or (!facing_right and move_dir > 0):
 		facing_right = !facing_right
-
 	
 	y_velo += GRAVITY
-
-	if (grounded or coyote_timer > 0) and Input.is_action_pressed("jump"):
-		y_velo = -JUMP_FORCE
-		coyote_timer = 0
-
+	
+	if grounded or coyote_timer > 0:
+		if Input.is_action_pressed("jump"):
+			if !is_jumping:
+				is_jumping = true
+				jump_timer = 0
+			jump_timer += delta
+			if jump_timer <= MAX_JUMP_TIME:
+				y_velo = -JUMP_FORCE
+		else:
+			is_jumping = false
+			jump_timer = 0
+	
 	if grounded and y_velo >= 5:
 		y_velo = 5
 	if y_velo > MAX_FALL_SPEED:
 		y_velo = MAX_FALL_SPEED
+
+
 		
 
